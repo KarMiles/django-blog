eb1f8cf0ac6e6444f4116832c7ca5b92909e0a8c
Karol Mileszko <karol.miles@gmail.com>
2022-06-03 08:39:58 +0000

    Add urls.py in blog folder

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,10 @@ from .models import Post
 
 # Create your views here.
 
-# New View Checklist - 1. Create the view code
+# New View Checklist:
+# 1. Create the view code (below)
+# 2. Create a template to render the view
+# 3. Connect up URLs in the urls.py file
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
